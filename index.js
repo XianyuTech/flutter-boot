4c4fab1345ee9bf03cd2d31a2bda43768888794a
voicewitness <voicewitness@gmail.com>
2019-09-11 21:23:15 +0800

    modify version

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ require('./src/commands/update')(program)
 require('./src/commands/use')(program)
 
 program
-  .version('0.0.4')
+  .version('0.0.5')
   .description('Flutter Boot: install && run')
   .parse(process.argv)
